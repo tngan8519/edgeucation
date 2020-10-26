20bac96ceca26812043c6076622bf7ce40866acb
tngan8519 <71539113+tngan8519@users.noreply.github.com>
2020-10-26 16:18:53 -0500

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,22 +3,28 @@ import React from "react";
 const Result = ({ data }) => {
   return (
     <section className="result">
-      <h4>Results:</h4>
-      <ol>
-        {data.map((x, index) => (
-          <li key={index}>
-            <div> {x["school.name"]}</div>
-            <div>State: {x["school.state"]}</div>
-            <div>
-              Tuition-in-state:
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(x["2018.cost.tuition.in_state"])}
-            </div>
-          </li>
-        ))}
-      </ol>
+      {data.length === 0 ? (
+        <h4>No result</h4>
+      ) : (
+        <>
+          <h4>Results:</h4>
+          <ol>
+            {data.map((x, index) => (
+              <li key={index}>
+                <div> {x["school.name"]}</div>
+                <div>State: {x["school.state"]}</div>
+                <div>
+                  Tuition-in-state:
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(x["2018.cost.tuition.in_state"])}
+                </div>
+              </li>
+            ))}
+          </ol>
+        </>
+      )}
     </section>
   );
 };
